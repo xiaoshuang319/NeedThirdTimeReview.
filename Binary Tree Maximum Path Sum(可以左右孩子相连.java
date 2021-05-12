8592d27b47fbeb5e082fd16205fbf38c36bf4b3a
xiaoshuang319 <xiaoshuang319@gmail.com>
2021-05-11 22:42:03 -0700

    Update Binary Tree Maximum Path Sum(可以左右孩子相连.java

--- Binary Tree Maximum Path Sum(可以左右孩子相连.java
+++ Binary Tree Maximum Path Sum(可以左右孩子相连.java
@@ -1,31 +1,25 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    int sum = Integer.MIN_VALUE;
     public int maxPathSum(TreeNode root) {
-        helper(root);
-        return sum;
+        int[]res = new int[1];
+        res[0] = Integer.MIN_VALUE;
+        maxPathFromRootToLeaf(root,res);
+        return res[0];
     }
-    private int helper(TreeNode root){
-        if(root == null){
+    private int maxPathFromRootToLeaf(TreeNode node, int[]res){
+        if(node == null){
             return 0;
         }
-        int leftSum = Math.max(0, helper(root.left));
-        int rightSum = Math.max(0, helper(root.right));
-        sum = Math.max(sum, root.val + leftSum + rightSum);
-        return Math.max(leftSum,rightSum) + root.val;
+        int leftSum = maxPathFromRootToLeaf(node.left, res);
+        int rightSum = maxPathFromRootToLeaf(node.right, res);
+        //if leftSum < 0,give up left
+        if(leftSum < 0){
+            leftSum = 0;
+        }
+        //if rightSum < 0,give up right
+        if(rightSum < 0){
+            rightSum = 0;
+        }
+        res[0] = Math.max(res[0], leftSum + rightSum + node.val);
+        return Math.max(leftSum, rightSum) + node.val;
     }
 }
