ab9e7bb682ce956cad5b011a480d10d68c632221
xiaoshuang319 <xiaoshuang319@gmail.com>
2021-05-12 16:31:14 -0700

    Create Range of BST.java

---
+++ Range of BST.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        if(root == null){
+            return 0;
+        }
+
+        if(root.val > high){
+          return rangeSumBST(root.left,low,high);
+        }
+        if(root.val < low){
+            return  rangeSumBST(root.right,low,high);
+        }
+        if(root.val >= low && root.val <= high)
+        {
+           int leftSum = rangeSumBST(root.left,low,high);
+           int rightSum =  rangeSumBST(root.right,low,high);
+           return leftSum + rightSum + root.val;
+        }
+        return 0;
+    }
+}
