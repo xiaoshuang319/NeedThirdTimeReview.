116435bd6cd6bfd85810720983faaa7dd683ff4a
xiaoshuang319 <xiaoshuang319@gmail.com>
2021-05-13 02:19:26 -0700

    Create interaction of two linkedList.java

---
+++ interaction of two linkedList.java
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        //as long as path headA walk through is same as path headB walk through, then they must meet
+        //if headA == null, then make it to walk through path headB walked,
+        ListNode currA = headA;
+        ListNode currB = headB;
+        while(currA != currB){
+            currA = currA == null ? headB : currA.next;
+            currB = currB == null ? headA : currB.next;
+        }
+        return currA;
+    }
+}
