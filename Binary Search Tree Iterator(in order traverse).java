824ff760b9e3e7db6c7aea79689952e0bf74e1f5
xiaoshuang319 <xiaoshuang319@gmail.com>
2021-05-12 22:40:50 -0700

    Update and rename Binary Search Tree Iterator.java to Binary Search Tree Iterator(in order traverse).java

--- Binary Search Tree Iterator(in order traverse).java
+++ Binary Search Tree Iterator(in order traverse).java
@@ -46,3 +46,49 @@ class BSTIterator {
  * int param_1 = obj.next();
  * boolean param_2 = obj.hasNext();
  */
+
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val,Node _left,Node _right) {
+        val = _val;
+        left = _left;
+        right = _right;
+    }
+};
+*/
+
+class Solution {
+    Node pre = null; //global variable
+    public Node treeToDoublyList(Node root) {
+      if(root == null)return null;
+      Node dummyNode = new Node(-1,null,null);
+      pre = dummyNode;
+      helper(root);
+      pre.right = dummyNode.right;
+      dummyNode.right.left = pre;
+      return dummyNode.right;
+      
+    }
+    private Node helper(Node node){
+        if(node == null)return null;
+        helper(node.left);
+        pre.right = node;
+        node.left = pre;
+        pre = pre.right;
+        helper(node.right);
+        return pre.right;
+    }
+    
+}
