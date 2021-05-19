b272663ffe26b05d4a6a86603165cd957e5a8cb4
xiaoshuang319 <xiaoshuang319@gmail.com>
2021-05-18 20:45:30 -0700

    Update isBipartite.java

--- isBipartite.java
+++ isBipartite.java
@@ -28,3 +28,36 @@ class Solution {
         return true;
     }
 }
+//union find
+class Solution {
+    int[] parent;
+    public boolean isBipartite(int[][] graph) {
+        int N = graph.length;
+        parent = new int[N];
+        for(int i = 0; i < N; i++)
+            parent[i] = i;
+        
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < graph[i].length; j++) {
+                if(find(i) == find(graph[i][j]))
+                    return false;
+                union(graph[i][0], graph[i][j]);
+            }
+        }
+        
+        return true;
+    }
+    
+    private int find(int x) {
+        while(x != parent[x]){
+            x = find(parent[x]);
+        }
+        return x;
+    }
+    
+    private void union(int x, int y) {
+        int parentX = find(x);
+        int parentY = find(y);
+        parent[parentY] = parentX;
+    }
+}
