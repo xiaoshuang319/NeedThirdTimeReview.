6cd39d429be3e1eb2b6082e87628e49ed082b503
xiaoshuang319 <xiaoshuang319@gmail.com>
2021-05-18 22:52:36 -0700

    Update mergeKLists(merge two lists).java

--- mergeKLists(merge two lists).java
+++ mergeKLists(merge two lists).java
@@ -30,3 +30,33 @@ class Solution {
         return dummyNode.next;
     }
 }
+
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if(lists == null || lists.length == 0)return null;
+        return helper(lists,0,lists.length - 1);
+    }
+    private ListNode helper(ListNode[]lists, int start, int end){
+        if(start == end){
+            return lists[start];
+        }
+        int mid = start + (end - start) / 2;
+        ListNode l1 = helper(lists,start, mid);
+        ListNode l2 = helper(lists,mid + 1, end);
+        return merge2List(l1,l2);
+    }
+    private ListNode merge2List(ListNode l1, ListNode l2){
+        if(l1 == null){
+            return l2;
+        }
+        if(l2 == null){
+            return l1;
+        }
+        if(l1.val < l2.val){
+            l1.next = merge2List(l1.next, l2);
+            return l1;
+        }else{
+            l2.next =  merge2List(l1, l2.next);
+            return l2;
+        }
+   
