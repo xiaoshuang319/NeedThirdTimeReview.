d5d9055706524f04af0aa15e7c71787e9013e88d
xiaoshuang319 <xiaoshuang319@gmail.com>
2021-05-25 23:07:03 -0700

    Create 快慢指针.java

---
+++ 快慢指针.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        //maitin distance with n between fast and slow
+        ListNode dummyNode = new ListNode(-1);
+        dummyNode.next = head;
+        ListNode fast = dummyNode;
+        ListNode slow = dummyNode;
+        for(int i = 0; i < n; i++){
+            fast = fast.next;
+        }
+        while(fast.next != null){
+            fast = fast.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+        return  dummyNode.next;
+    }
+}
