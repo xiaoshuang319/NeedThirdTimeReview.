467c65dcad860e5b1fec61f496d77ee645920f1a
xiaoshuang319 <xiaoshuang319@gmail.com>
2021-05-26 20:11:58 -0700

    Update Populating Next Right Pointers in Each Node.java

--- Populating Next Right Pointers in Each Node.java
+++ Populating Next Right Pointers in Each Node.java
@@ -35,3 +35,53 @@ class Solution {
         helper(currNode.right, currNode.next == null ? null : currNode.next.left);
     }
 }
+//
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    public Node connect(Node root) {
+        Node dummyNode = new Node(-1);//keep the first node at each layer
+        Node pre = dummyNode;
+        Node curr = root;
+        while(curr != null){
+            if(curr.left != null){
+                pre.next = curr.left;
+                pre = pre.next;
+            }
+            if(curr.right != null){
+                pre.next = curr.right;
+                pre = pre.next;
+            } 
+            curr = curr.next;
+            if(curr == null){
+                pre = dummyNode;
+                curr =  dummyNode.next;
+                dummyNode.next = null;
+                
+            }
+        }
+        return root;
+        
+    }
+}
